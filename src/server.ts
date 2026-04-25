1d287847c7eb44092c70137bd9234899d031a114
luiz-divino <nandodivinodev@gmail.com>
2026-04-25 00:36:53 -0300

    feat(server): initialize Express server with middleware and port configuration

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import './util/module-alias'
+import "dotenv/config";
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors)
+
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`);
+})
