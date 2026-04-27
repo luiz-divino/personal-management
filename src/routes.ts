33dceececbe0e52b695229177f91e774c6089423
luiz-divino <nandodivinodev@gmail.com>
2026-04-27 19:06:41 -0300

    feat(user): add route for user registration with schema validation

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,14 @@
 import { Router } from "express";
+import { validateUserSchema } from "./middlewares/validateUserSchema";
+import { createUserSchema } from "./schemas/userSchema";
+import { CreateUserController } from "./controllers/CreateUserController";
+
 const router = Router();
+
+router.post(
+  "/users",
+  validateUserSchema(createUserSchema),
+  new CreateUserController().handle,
+);
 
 export { router };
