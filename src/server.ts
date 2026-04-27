eb8a674aec79972a2c780d702fc3f4368e3b4608
luiz-divino <nandodivinodev@gmail.com>
2026-04-27 19:08:16 -0300

    refactor(api): reorganize middleware execution order and register main router

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import './util/module-alias'
 import "dotenv/config";
 import express from 'express';
 import cors from 'cors';
+import { router } from './routes';
 
 const app = express();
 app.use(express.json());
-app.use(cors)
+app.use(cors())
+app.use(router)
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
-})
+})          
