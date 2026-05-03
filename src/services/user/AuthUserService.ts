a0e9e15243d289f415aec51595edac3b57b7e6f1
luiz-divino <nandodivinodev@gmail.com>
2026-05-02 23:56:59 -0300

    feat(auth): implement AuthUserService for JWT-based user authentication

---
+++ src/services/user/AuthUserService.ts
@@ -0,0 +1,45 @@
+import prismaClient from "@src/prisma";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+
+interface AuthUserServiceProps {
+  email: string;
+  password: string;
+}
+
+class AuthUserService {
+  async execute({ email, password }: AuthUserServiceProps) {
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    if (!user) {
+      throw new Error("email and password are required");
+    }
+    const passwordMatch = await compare(password, user.password);
+    if (!passwordMatch) {
+      throw new Error("email and password are required");
+    }
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET as string,
+      {
+        subject: user.id,
+        expiresIn: "30d",
+      },
+    );
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token
+    }
+  }
+}
+
+const authUserService = new AuthUserService();
+export { authUserService };
