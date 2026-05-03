c7aa4cb4ec9c54ba7e2c5b12e10306646de42757
luiz-divino <nandodivinodev@gmail.com>
2026-05-02 23:47:54 -0300

    feat(auth): implement AuthUserController for user authentication

---
+++ src/controllers/user/AuthUserController.ts
@@ -0,0 +1,20 @@
+import { authUserService } from "@src/services/user/AuthUserService";
+import { Request, Response } from "express";
+
+class AuthUserController {
+  async handle(req: Request, res: Response) { 
+    const { email, password } = req.body;
+    try {
+      const userLogin = await authUserService.execute({email, password})
+      res.json({
+       userLogin
+      })
+    } catch (error) {
+      res.status(400).json(error)
+    }
+    res.status(500).json("internal server error")
+  }
+}
+
+const authUserController = new AuthUserController();
+export { authUserController };
