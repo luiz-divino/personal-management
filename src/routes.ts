7c0ad82799d7ffe5ab138566355689d1ce805e39
luiz-divino <nandodivinodev@gmail.com>
2026-05-04 22:56:10 -0300

    feat(routes): add login route

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { validateUserSchema } from "./middlewares/validateUserSchema";
-import { createUserSchema } from "./schemas/userSchema";
-import { CreateUserController } from "./controllers/CreateUserController";
+import { LoginUserSchema, createUserSchema } from "./schemas/userSchema";
+import { CreateUserController } from "@src/controllers/user/CreateUserController";
+import { authUserController } from "@src/controllers/user/AuthUserController";
 
 const router = Router();
 
@@ -9,6 +10,12 @@ router.post(
   "/users",
   validateUserSchema(createUserSchema),
   new CreateUserController().handle,
+);
+
+router.post(
+  "/session",
+  validateUserSchema(LoginUserSchema),
+  authUserController.handle,
 );
 
 export { router };
